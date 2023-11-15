7548e9ddeb44901f47bcf7b196e4c33d8ab31093
NotCloudy <147564854+NotCloudy@users.noreply.github.com>
2023-11-14 17:56:34 -0700

    hi

--- pytgon.py
+++ pytgon.py
@@ -1,26 +1,69 @@
 import random
+def game():
+    rock = "r"
+    paper = "p"
+    scissors = "s"
+    options = ["r","p","s"]
+    humanCounter = 0
+    aiCounter = 0
+    aiTurn = random.choice(options)
+    humanTurn = input("-rock papers scissors-")
 
-print("hello world")
-rock = "r"
-paper = "p"
-scissors = "s"
-options = ["r","p","s"]
 
-aiTurn = random.choice(options)
-humanTurn = input("-rock papers scissors-")
+    def winConditions(humanCounter, aiCounter):
+        if humanTurn == "r" and aiTurn == "s":
+            print(aiTurn)
+            print("You win!")
+            humanCounter = humanCounter + 1
+        elif humanTurn == "s" and aiTurn == "p":
+            print(aiTurn)
+            print("You win!")
+            humanCounter = humanCounter + 1
+        elif humanTurn == "p" and aiTurn == "r":
+            print(aiTurn)
+            print("You win!")
+            humanCounter = humanCounter + 1
+        elif aiTurn == "r" and humanTurn == "s":
+            print(aiTurn)
+            print("You lose!")
+            aiCounter = aiCounter + 1
+        elif aiTurn == "s" and humanTurn == "p":
+            print(aiTurn)
+            print("You lose!")
+            aiCounter = aiCounter + 1
+        elif aiTurn == "p" and humanTurn == "r":
+            print(aiTurn)
+            print("You lose!")
+            aiCounter = aiCounter + 1
+    winConditions(humanCounter, aiCounter)
+    def tieConditions():
+        if humanTurn == "r" and aiTurn == "r":
+            print(aiTurn)
+            print("Tie!")
+        elif humanTurn == "p" and aiTurn == "p":
+            print(aiTurn)
+            print("Tie!")
+        elif humanTurn == "s" and aiTurn == "s":
+            print(aiTurn)
+            print("Tie!")
+    tieConditions()
+
+game()
+
+print(' Next round! ')
+game()
+print(' Next round! ')
+game()
+print(' Next round! ')
+game()
+
+def win(humanCounter, aiCounter):
+    if humanCounter > aiCounter:
+        print("you win!!!!11!!!!!!!1")
+    elif humanCounter < aiCounter:
+        print("you lose !!!!!11!!!!!!1")
+win()
 
 
-def winConditions():
-    if humanTurn == "r" and aiTurn == "s":
-        print("You win!")
-    elif humanTurn == "s" and aiTurn == "p":
-        print("You win!")
-    elif humanTurn == "p" and aiTurn == "r":
-        print("You win!")
 
-    elif aiTurn == "r" and humanTurn == "s":
-        print("You lose!")
-    elif aiTurn == "s" and humanTurn == "p":
-        print("You lose!")
-    elif aiTurn == "p" and humanTurn == "r":
-        print("You lose!")
+
